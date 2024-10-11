8da9e8168bf38cfeb85332582e1e3d89f2771d53
yamao-sys <yamaopanku@gmail.com>
2024-10-11 09:58:51 +0900

    userのCRUD

--- app/main.go
+++ app/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
-	"app/config"
+	"app/models"
 	"fmt"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DBName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DBName)
+	// fmt.Println(config.Config.LogFile)
+
+	// log.Println("test")
+
+	fmt.Println(models.Db)
+
+	/*
+		// ユーザ作成
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.Password = "testtest"
+		fmt.Println(u)
+
+		u.CreateUser()
+	*/
+
+	/*
+		// ユーザ更新
+		u, _ := models.GetUser(1)
+		fmt.Println(u)
+
+		u.Name = "test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+	*/
+
+	/*
+		// ユーザ削除
+		u, _ := models.GetUser(1)
+		fmt.Println(u)
+
+		u.DeleteUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+	*/
 }
---
+++ app/models/base.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"app/config"
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var Db *sql.DB
+
+var err error
+
+const (
+	tableNameUser = "users"
+)
+
+func init() {
+	Db, err = sql.Open(config.Config.SQLDriver, config.Config.DBName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		uuid STRING NOT NULL UNIQUE,
+		name STRING,
+		email STRING,
+		password STRING,
+		created_at DATETIME)`, tableNameUser)
+
+	Db.Exec(cmdU)
+}
+
+func CreateUUID() (uuidobj uuid.UUID) {
+	uuidobj, _ = uuid.NewUUID()
+	return uuidobj
+}
+
+func Encrypt(plainText string) (crypText string) {
+	crypText = fmt.Sprintf("%x", sha1.Sum([]byte(plainText)))
+	return crypText
+}
+
+func main() {}
